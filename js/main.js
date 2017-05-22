69323e3df4c1e159820b899700200c8aac334719
konglobemeralt <jesper.blidkvist@live.se>
2017-05-22 18:08:47 +0200

    Added avatar, implemented old shake script

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 
  var $container = $('.portfolio-items').isotope('layout');
 
-$(window).load(function(){
-     $container.isotope({
+
+   
+   
+$(document).ready(function(){
+    //shake
+	$(".avatar").click(function(){
+        $(this).shake(2, 10, 400 );
+	});
+	  $container.isotope({
      filter: '.recent'
     });
-   });
+
+});
 
 
 // filter items on button click
@@ -17,3 +25,16 @@ $('.portfolio-filter').on( 'click', 'a', function(e) {
 	$('.portfolio-filter li').removeClass('active');
 	$(this).closest('li').addClass('active');
 });
+
+jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+        $(this).css("position","relative"); 
+        for (var x=1; x<=intShakes; x++) {
+        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+    .animate({left:intDistance}, ((intDuration/intShakes)/2))
+    .animate({left:0}, (((intDuration/intShakes)/4)));
+    }
+  });
+return this;
+};
+
